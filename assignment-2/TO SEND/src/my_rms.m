733e436327a29044071d6c4dd2f37a40222b30ce
Tudor Dragan <me@tudordev.com>
2015-03-10 18:25:45 +0100

    report

--- assignment-2/TO SEND/src/my_rms.m
+++
@@ -1,6 +0,0 @@
-function error = my_rms( predicted, measured)
-%MY_RMS Summary of this function goes here
-%   Detailed explanation goes here
-    error = sqrt(1/length(predicted) * sum((measured - predicted).^2));
-end
-
--- assignment-2/TO SEND/src/my_wML.m
+++
@@ -1,6 +0,0 @@
-function output = my_wML( Phi, T )
-%MY_WML Summary of this function goes here
-%find maximum likelihood estimates
-    output = pinv(Phi'*Phi) * (Phi'* T);
-end
-
